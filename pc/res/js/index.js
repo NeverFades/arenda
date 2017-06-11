767f16fd9e665bd4dd3ec49681c9761481bde92d
韩梦晴 <657691495@qq.com>
2017-06-11 15:44:33 +0800

    格式化代码

--- pc/res/js/index.js
+++ pc/res/js/index.js
@@ -1,34 +1,6 @@
-$(document).ready(function () {
-    jQuery(".slideBox").slide({
-        mainCell: ".bd ul",
-        autoPlay: true,
-        effect: 'fade',
-        trigger: 'mouseover',
-        easing: 'swing',
-        delayTime: 500,
-        mouseOverStop: true,
-        pnLoop: true
-    });
-    jQuery(".picScroll-left").slide({
-        titCell: ".hd ul",
-        mainCell: ".bd ul",
-        autoPage: true,
-        effect: 'left',
-        autoPlay: true,
-        scroll: 1,
-        vis: 5,
-        easing: 'swing',
-        delayTime: 500,
-        pnLoop: true,
-        trigger: 'mouseover',
-        mouseOverStop: true
-    });
-    $('.nav-hd').find('li').click(function () {
-        $(this).addClass('on').siblings('li').removeClass('on');
-    });
-    $('.base-warp_bd').find('li').click(function () {
-        $(this).addClass('on').siblings('li').removeClass('on');
-        $('.base-warp').find('.base-desc').removeClass('js-show');
-        $('#' + $(this).attr('data-target')).addClass('js-show');
-    })
-});
+/*!
+ * Copyright 2017 Qiuhua Jia.
+ * @author hua <flower346@qq.com>
+ */
+/*--------------------------------*/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('$(A).z(4(){k(".B").j({i:".l b",m:0,o:\'D\',d:\'t\',c:\'h\',f:g,u:0,e:0});k(".C-n").j({y:".v b",i:".l b",I:0,o:\'n\',m:0,F:1,E:5,c:\'h\',f:g,e:0,d:\'t\',u:0});$(\'.G-v\').7(\'2\').p(4(){$(8).a(\'3\').q(\'2\').9(\'3\')});$(\'.6-H\').7(\'2\').p(4(){$(8).a(\'3\').q(\'2\').9(\'3\');$(\'.6-L\').7(\'.6-K\').9(\'s-r\');$(\'#\'+$(8).J(\'x-w\')).a(\'s-r\')})});',48,48,'true||li|on|function||base|find|this|removeClass|addClass|ul|easing|trigger|pnLoop|delayTime|500|swing|mainCell|slide|jQuery|bd|autoPlay|left|effect|click|siblings|show|js|mouseover|mouseOverStop|hd|target|data|titCell|ready|document|slideBox|picScroll|fade|vis|scroll|nav|warp_bd|autoPage|attr|desc|warp'.split('|'),0,{}))
